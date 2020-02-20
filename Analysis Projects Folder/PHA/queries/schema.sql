912f01f0b99c8e10649d0b9cbb465d0aaf34cc3c
TL <tom.kennedy.lee@gmail.com>
2020-02-20 08:37:20 -0500

    retirement_info table

--- Analysis Projects Folder/PHA/queries/schema.sql
+++ Analysis Projects Folder/PHA/queries/schema.sql
@@ -55,3 +55,21 @@ CREATE TABLE titles (
 		FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 		PRIMARY KEY (emp_no)
 );
+
+
+-- 02/20/2020
+
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_day BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- query and save into a table
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_day BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
